acfc9a0f84303f84d527a0e4f79bde6af6fa1544
kaisar Hameed <kaisersofi.ks2515@gmail.com>
2025-08-30 15:23:35 +0530

    added checkbox drawer item

---
+++ src/app/(drawer)/checkbox-interactions/index.tsx
@@ -0,0 +1,5 @@
+import { CheckboxInteractions as CheckboxInteractionsScreen } from "@/screens/checkbox-interactions";
+
+export default function CheckboxInteractions() {
+  return <CheckboxInteractionsScreen />;
+}
